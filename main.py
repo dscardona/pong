ccdeab4c69824b8dc4744c621d68058292d4ffba
Diana Cardona <dscardona@gmail.com>
2023-08-23 12:23:11 -0400

    Turn off animation and refresh manually

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
 
 paddle = Paddle()
+screen.update()
 
 screen.listen()
 screen.onkey(paddle.up, "Up")
