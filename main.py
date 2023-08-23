3be9b9c0fac51edc1107c47f0bbc33ae61f57e77
Diana Cardona <dscardona@gmail.com>
2023-08-23 11:11:02 -0400

    Create paddle class, instantiate it

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+
+paddle = Paddle()
+
 
 
 
